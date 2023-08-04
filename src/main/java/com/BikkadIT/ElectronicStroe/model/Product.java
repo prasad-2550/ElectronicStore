2661cd153b0e2cf6b868f0b8f429be7bfeedf544
prasad-2550 <pawar.prasad5025@gmail.com>
2023-08-04 13:34:09 +0530

    Product done

--- src/main/java/com/BikkadIT/ElectronicStroe/model/Product.java
+++ src/main/java/com/BikkadIT/ElectronicStroe/model/Product.java
@@ -1,9 +1,6 @@
 package com.BikkadIT.ElectronicStroe.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -11,6 +8,7 @@ import java.util.Date;
 @Setter
 @Getter
 @AllArgsConstructor
+@Builder
 @NoArgsConstructor
 @Entity
 @Table(name = "product")
